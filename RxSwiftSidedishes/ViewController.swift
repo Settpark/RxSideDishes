8024643f604fadd2f2e6a0ac5bc70f5916635f8e
Sett Park <park9368@naver.com>
2021-07-26 14:29:51 +0900

    init: 프로젝트 초기화

---
+++ RxSwiftSidedishes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftSidedishes
+//
+//  Created by 박정하 on 2021/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
